163830a6629aad2b014de7d64b90e6cec5d36e53
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-28 07:48:49 +0530

    context3 example completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,108 @@
-import React from "react";
-import "./style.css";
-
+import React, { useState, useEffect, useContext, createContext } from 'react';
+import axios from 'axios';
+const OnlineContext = createContext();
 export default function App() {
+  const [a, seta] = useState(null);
+  const fetchData = (url) => {
+    axios
+      .get(url)
+      .then((res) => res.data)
+      .then((d) => seta(d));
+  };
+  const value = {
+    a,
+    fetchData,
+  };
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <OnlineContext.Provider value={value}>
+      <Posts />
+      <Comments />
+      <Albums />
+      <Photos />
+      <Users />
+      <Todos />
+      <Table />
+    </OnlineContext.Provider>
   );
 }
+const Posts = () => {
+  const data = useContext(OnlineContext);
+  // console.log(data);
+  const p1 = () => {
+    data.fetchData('https://jsonplaceholder.typicode.com/posts');
+  };
+  return (
+    <div>
+      <button onClick={p1}>Posts</button>
+    </div>
+  );
+};
+const Comments = () => {
+  const data = useContext(OnlineContext);
+  // console.log(data);
+  const p1 = () => {
+    data.fetchData('https://jsonplaceholder.typicode.com/comments');
+  };
+  return (
+    <div>
+      <button onClick={p1}>Comments</button>
+    </div>
+  );
+};
+const Albums = () => {
+  const data = useContext(OnlineContext);
+  // console.log(data);
+  const p1 = () => {
+    data.fetchData('https://jsonplaceholder.typicode.com/albums');
+  };
+  return (
+    <div>
+      <button onClick={p1}>Albums</button>
+    </div>
+  );
+};
+const Photos = () => {
+  const data = useContext(OnlineContext);
+  // console.log(data);
+  const p1 = () => {
+    data.fetchData('https://jsonplaceholder.typicode.com/photos');
+  };
+  return (
+    <div>
+      <button onClick={p1}>Photos</button>
+    </div>
+  );
+};
+const Users = () => {
+  const data = useContext(OnlineContext);
+  // console.log(data);
+  const p1 = () => {
+    data.fetchData('https://jsonplaceholder.typicode.com/users');
+  };
+  return (
+    <div>
+      <button onClick={p1}>Users</button>
+    </div>
+  );
+};
+const Todos = () => {
+  const data = useContext(OnlineContext);
+  // console.log(data);
+  const p1 = () => {
+    data.fetchData('https://jsonplaceholder.typicode.com/todos');
+  };
+  return (
+    <div>
+      <button onClick={p1}>Todos</button>
+    </div>
+  );
+};
+const Table = () => {
+  const data = useContext(OnlineContext);
+  return (
+    <div>
+      <pre>outout json in stringify format here</pre>
+      <pre>{JSON.stringify(data.a, null, 2)}</pre>
+    </div>
+  );
+};
