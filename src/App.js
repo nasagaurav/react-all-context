45eccd90b4be483a110ccbe3a72fff47db2062c5
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-28 07:21:01 +0530

    context completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from "react";
-import "./style.css";
-
+import React, { createContext, useContext } from 'react';
+import './style.css';
+const NasaContext = createContext();
 export default function App() {
+  const value = {
+    x: 1,
+    y: 2,
+    z: 3,
+    a: [1, 2, 3, 4, 5, 6, 7, 8],
+    b: ['html', 'css'],
+  };
+  return (
+    <NasaContext.Provider value={value}>
+      <C1 />
+    </NasaContext.Provider>
+  );
+}
+
+const C1 = () => <C2 />;
+const C2 = () => <C3 />;
+const C3 = () => <C4 />;
+const C4 = () => <C5 />;
+const C5 = () => <C6 />;
+const C6 = () => <C7 />;
+const C7 = () => <C8 />;
+const C8 = () => <C9 />;
+const C9 = () => <C10 />;
+
+function C10() {
+  const data = useContext(NasaContext);
+  console.log(data);
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>C10 component</h1>
     </div>
   );
 }
